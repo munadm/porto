eaf985b883ad55c44b385e67b45e80127ae41fb4
Munad Mahinoor <contact@munadmahinoor.com>
2021-11-13 19:45:41 -0500

    feat(NavHeader and Footer): add header and footer components

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import NavHeader from '../components/NavHeader'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <NavHeader />
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Time to learn{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
@@ -66,17 +68,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer/>
     </div>
   )
 }
